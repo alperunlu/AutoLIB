39ff372dec7afbda76d3b2012e04e668e8eb06a0
Alper Ünlü <alperunlu@ieee.org>
2021-10-31 03:10:06 +0300

    Update slblocks.m

--- slblocks.m
+++ slblocks.m
@@ -1,13 +1,7 @@
 function blkStruct = slblocks
-% This function specifies that the library 'mylib'
-% should appear in the Library Browser with the 
-% name 'My Library'
 
     Browser.Library = 'AutoLIB';
-    % 'mylib' is the name of the library
 
     Browser.Name = 'AutoLIB';
-    % 'My Library' is the library name that appears
-    % in the Library Browser
 
     blkStruct.Browser = Browser;
